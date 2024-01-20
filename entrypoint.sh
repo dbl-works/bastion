a5ca014a475bb6a5e93c08be7db8d6031e8d1009
Lud <lud@reinmiedl.com>
2024-01-20 14:55:48 +0100

    prevent polluting global namespace with sensitive information

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,31 +5,38 @@
 #   PERMITTED_GITHUB_USERNAMES - List of github usernames that can use this host
 #
 
-AUTHORIZED_KEYS=""
+# keep in a function to avoid polluting the global namespace with variables
+# that contain sensitive data
+function pull_ssh_keys_from_github_and_write_to_authorized_keys() {
+  local AUTHORIZED_KEYS=""
 
-#
-# to test this in zsh, run `for usr in $=PERMITTED_GITHUB_USERNAMES`
-# note the extra `=` sign that enables splitting by whitespace
-# read https://zsh.sourceforge.io/Doc/Release/Expansion.html#Parameter-Expansion
-#
-for usr in $PERMITTED_GITHUB_USERNAMES
-do
-  echo "Fetching keys for user: $usr"
-  KEYS=$(curl -s https://github.com/$usr.keys)
-  echo "Found a total of $(echo $KEYS | wc -w | xargs) keys"
+  #
+  # to test this in zsh, run `for usr in $=PERMITTED_GITHUB_USERNAMES`
+  # note the extra `=` sign that enables splitting by whitespace
+  # read https://zsh.sourceforge.io/Doc/Release/Expansion.html#Parameter-Expansion
+  #
+  for usr in $PERMITTED_GITHUB_USERNAMES
+  do
+    echo "Fetching keys for user: $usr"
+    local KEYS=$(curl -s https://github.com/$usr.keys)
+    echo "Found a total of $(echo $KEYS | wc -w | xargs) keys"
 
-  # Alpine/POSIX compliant way of adding new lines, since "\n" is treated as a literal character
-  # also: must left-align here to avoid extra whitespace the beginning of each line
-comment="# github.com/$usr"
-keys_block="$comment
+    # Alpine/POSIX compliant way of adding new lines, since "\n" is treated as a literal character
+    # also: must left-align here to avoid extra whitespace at the beginning of each line
+    local comment="# github.com/$usr"
+    local keys_block="$comment
 $KEYS
 
 "
 
-  AUTHORIZED_KEYS="${AUTHORIZED_KEYS}${keys_block}"
-done
+    AUTHORIZED_KEYS="${AUTHORIZED_KEYS}${keys_block}"
+  done
 
-echo "$AUTHORIZED_KEYS" > ~/.ssh/authorized_keys
+  echo "$AUTHORIZED_KEYS" > ~/.ssh/authorized_keys
+  echo "DONE"
+}
+
+pull_ssh_keys_from_github_and_write_to_authorized_keys
 
 # Run command as is passed in
 exec "$@"
