d3618398f39aef99876d0b5f41b740a2c8f529d7
Lud <lud@reinmiedl.com>
2024-01-21 00:56:45 +0100

    Alpine 3.19, drop ssh-dss, cleanup (#6)
    * bump year in licencse to 2024
    
    * bump alpine to 3.19
    
    * remove insecure ssh-dss from allowed key types
    
    * clear apk cache to reduce file size
    
    * document deployment on AWS ECS
    
    * better document entrypoint code
    
    * remove unmaintained changelog, we use github releases
    
    * prevent polluting global namespace with sensitive information
    
    * log when pulling from github starts/finishes

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,18 @@
-FROM alpine:3.18
+FROM alpine:3.19
 WORKDIR /root
 
 # Update core packages
 RUN apk update && apk upgrade
 
-RUN apk add openssh curl
+RUN apk add openssh curl \
+  && rm -rf /var/cache/apk/*
 
 RUN ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa
 RUN echo "root:$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 36 ; echo '')" | chpasswd
 
 COPY entrypoint.sh .
 COPY sshd_config /etc/ssh/sshd_config
+
 RUN mkdir ~/.ssh \
  && chmod 0700 ~/.ssh \
  && touch ~/.ssh/authorized_keys \
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,46 @@
 #!/bin/sh
+
+#
 # Environment Variables:
 #   PERMITTED_GITHUB_USERNAMES - List of github usernames that can use this host
+#
 
-AUTHORIZED_KEYS=""
+# keep in a function to avoid polluting the global namespace with variables
+# that contain sensitive data
+function pull_ssh_keys_from_github_and_write_to_authorized_keys() {
+  local AUTHORIZED_KEYS=""
 
-for USERNAME in $PERMITTED_GITHUB_USERNAMES
-do
-  # echo "USERNAME: $USERNAME"
-  KEYS=$(curl -s https://github.com/$USERNAME.keys)
-  AUTHORIZED_KEYS="$AUTHORIZED_KEYS# github.com/$USERNAME
+  #
+  # to test this in zsh, run `for usr in $=PERMITTED_GITHUB_USERNAMES`
+  # note the extra `=` sign that enables splitting by whitespace
+  # read https://zsh.sourceforge.io/Doc/Release/Expansion.html#Parameter-Expansion
+  #
+  for usr in $PERMITTED_GITHUB_USERNAMES
+  do
+    echo "Fetching keys for user: $usr"
+    local KEYS=$(curl -s https://github.com/$usr.keys)
+    echo "Found a total of $(echo $KEYS | wc -w | xargs) keys"
+
+    # Alpine/POSIX compliant way of adding new lines, since "\n" is treated as a literal character
+    # also: must left-align here to avoid extra whitespace at the beginning of each line
+    local comment="# github.com/$usr"
+    local keys_block="$comment
 $KEYS
 
 "
-done
 
-echo "$AUTHORIZED_KEYS" > ~/.ssh/authorized_keys
+    AUTHORIZED_KEYS="${AUTHORIZED_KEYS}${keys_block}"
+  done
+
+  echo "$AUTHORIZED_KEYS" > ~/.ssh/authorized_keys
+  echo "DONE"
+}
+
+echo "Initializing SSH keys..."
+
+pull_ssh_keys_from_github_and_write_to_authorized_keys
+
+echo "Ready to serve incoming SSH connections."
 
 # Run command as is passed in
 exec "$@"
